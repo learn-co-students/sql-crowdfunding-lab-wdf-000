f2a09980ac864506364c43c05b973938e42724c1
satub <satu.barnhill@gmail.com>
2016-08-03 15:41:46 -0400

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO users (name, age) VALUES
+("Anna", 20),("Anna", 20),("Anna", 20),("Anna", 20),
+("Anna", 20),("Anna", 20),("Anna", 20),("Anna", 20),
+("Anna", 20),("Anna", 20),("Anna", 20),("Anna", 20),
+("Anna", 20),("Anna", 20),("Anna", 20),("Anna", 20),
+("Anna", 20),("Anna", 20),("Anna", 20),("Anna", 20);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Introduction to Xeroxlogy", "copy-paste-icine", 600, "now", "9999-12-31 23:59:59"),
+("Introduction to Xeroxlogy", "copy-paste-icine", 600, "now", "9999-12-31 23:59:59"),
+("Introduction to Xeroxlogy", "copy-paste-icine", 600, "now", "9999-12-31 23:59:59"),
+("Introduction to Xeroxlogy", "copy-paste-icine", 600, "now", "9999-12-31 23:59:59"),
+("Introduction to Xeroxlogy", "copy-paste-icine", 600, "now", "9999-12-31 23:59:59"),
+("Introduction to Xeroxlogy", "copy-paste-icine", 600, "now", "9999-12-31 23:59:59"),
+("Introduction to Xeroxlogy", "copy-paste-icine", 600, "now", "9999-12-31 23:59:59"),
+("Introduction to Xeroxlogy", "copy-paste-icine", 600, "now", "9999-12-31 23:59:59"),
+("Introduction to Xeroxlogy", "copy-paste-icine", 600, "now", "9999-12-31 23:59:59"),
+("Introduction to Xeroxlogy", "copy-paste-icine", 600, "now", "9999-12-31 23:59:59");
+
+INSERT INTO pledges (user_id, project_id, amount) VALUES
+(1,1,10),(1,2,10),(3,10,10),(5,6,20),(10,5,30),(1,1,10),(1,2,10),(3,10,10),(5,6,20),(10,5,30),
+(1,1,10),(1,2,10),(3,10,10),(5,6,20),(10,5,30),(1,1,10),(1,2,10),(3,10,10),(5,6,20),(10,5,30),
+(1,1,10),(1,2,10),(3,10,10),(5,6,20),(10,5,30),(1,1,10),(1,2,10),(3,10,10),(5,6,20),(10,5,30);
